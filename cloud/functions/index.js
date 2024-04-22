41ccd9eeb0a0ec0e257158841f42f52f5b768749
Mohamed abo el maaty zaky <98196426+MohamedAboElM3aTy@users.noreply.github.com>
2024-04-22 11:54:19 +0200

    add: Cloud Function

--- cloud/functions/index.js
+++ cloud/functions/index.js
@@ -4,7 +4,7 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 exports.sendRideRequestNotification = functions.firestore
-    .document("location/{docId}")
+    .document("rideRequests/{docId}")
     .onWrite(async (change, context) => {
       const locationData = change.after.data();
       const message = {
@@ -12,14 +12,10 @@ exports.sendRideRequestNotification = functions.firestore
         title: "New Ride Request!",
           body: `A new Client Requests a ride to ${locationData.headLocation}`,
         },
-        // data: {
-          // userLatitude: locationData.userLatitude,
-          // userLongitude: locationData.userLongitude,
-          // headLatitude: locationData.headLatitude,
-          // headLongitude: locationData.headLongitude,
-          // headLocation: locationData.headLocation,
-        // },
-        topic: "location",
+        data: {
+          "Ride Request": locationData.headLocation,
+        },
+        topic: "rideRequests",
       };
 
       try {
@@ -32,7 +28,7 @@ exports.sendRideRequestNotification = functions.firestore
 
 
 exports.sendRideResponseNotification = functions.firestore
-    .document("location/{docId}")
+    .document("rideRequests/{docId}")
     .onUpdate(async (change, context) => {
       const locationData = change.after.data();
 
@@ -41,7 +37,10 @@ exports.sendRideResponseNotification = functions.firestore
         title: "Response to Ride Request!",
           body: `The driver accepted your ride to ${locationData.headLocation}`,
         },
-        topic: "location",
+        data: {
+          "Ride Location": locationData.headLocation,
+        },
+        topic: "rideRequests",
       };
 
       try {
